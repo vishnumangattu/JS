f198864d5aa49d0521883a58c915f70a70964453
vishnumangattu <vishnumangattu03@gmail.com>
2024-07-20 11:01:15 +0530

    ss

---
+++ js/builtfn.js
@@ -0,0 +1,24 @@
+let a=" hello world "
+console.log(a.length);
+console.log(a.toUpperCase());
+console.log(a.toLowerCase());
+console.log(a.toUpperCase().toLowerCase().length);
+console.log(a.trim())
+console.log(a.trimLeft())
+console.log(a.trimRight())
+console.log(a.substring(0,5))
+console.log(a.replace("hello","hai"))
+console.log(a.startsWith(" hello"));
+console.log(a.endsWith("world "));
+ar=a.split("o")
+console.log(ar);
+str=ar.join("o")
+console.log(str);
+console.log(a.charAt("4"));
+console.log(a.slice(2,));
+
+// Array
+
+
+
+
